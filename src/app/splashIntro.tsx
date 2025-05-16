73b181fe2e7cb69c174a66c3c2c0d95b268bf109
Cal <cydoval@mac.com>
2025-05-16 15:17:12 -0700

    caes

--- src/app/splashIntro.tsx
+++ src/app/splashIntro.tsx
@@ -4,9 +4,7 @@ const SplashIntro: React.FC = () => {
       <div className="container">
         <div className="lg:w-1/2 h-[80vh] flex flex-col justify-between bg-hildgard bg-opacity-88">
           <div className="mt-auto">
-            <h1 className="text-2xl font-bold mb-4">
-              Environmental Science and Policy
-            </h1>
+            <h1 className="text-2xl font-bold mb-4">Hildgard631</h1>
             <p className="text-base leading-relaxed">
               Crafted just beyond the campus gates, Hildegard631 offers
               small-batch wines born from a passion for fermentation, flavor,
