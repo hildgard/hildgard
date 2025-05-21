c1236451cc715074397421b40e89843fe8884dae
Cal <cydoval@mac.com>
2025-05-21 14:19:36 -0700

    g analytics

--- src/app/splashIntro.tsx
+++ src/app/splashIntro.tsx
@@ -2,11 +2,12 @@ const SplashIntro: React.FC = () => {
   return (
     <div className="intro-wrapper h-[100vh]">
       <div className="container">
-        <div className="lg:w-1/2 h-[80vh] flex flex-col justify-between bg-hildgard bg-opacity-88">
-          <div className="mt-auto">
-            <h1 className="text-2xl font-bold mb-4">Hildgard631</h1>
+        <div className="lg:w-1/3 h-[85vh] flex flex-col justify-between bg-hildgard splash-red">
+          <div className="mt-auto p-12 text-white">
+            <p className="text-md italic">Introducing</p>
+            <h1 className="text-2xl font-bold mb-4">Hilgard631</h1>
             <p className="text-base leading-relaxed">
-              Crafted just beyond the campus gates, Hildegard631 offers
+              Crafted just beyond the campus gates, Hilgard631 offers
               small-batch wines born from a passion for fermentation, flavor,
               and thoughtful process.
             </p>
