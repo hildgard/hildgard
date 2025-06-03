f40bfcb42bb6d853a55a5bcc7de45043a6518e36
Cal <cydoval@mac.com>
2025-06-02 17:23:31 -0700

    selectioñ

--- src/app/components/navItems.tsx
+++ src/app/components/navItems.tsx
@@ -4,20 +4,25 @@ const NavItems: React.FC = () => {
   return (
     <nav>
       <Link
-        className="font-bold text-white border active:bg-primary-dark hover:bg-primary-light border-white py-1 px-5"
+        className="font-bold text-nav pe-2 me-2 active:text-white active:underline hover:text-white"
         href="/#about"
       >
         About
       </Link>
       <Link
-        className="font-bold text-white border active:bg-primary-dark hover:bg-primary-light border-white py-1 px-5 md:mx-4"
+        className="font-bold text-nav pe-2 me-2 active:text-white active:underline hover:text-white"
+        href="/#selection"
+      >
+        Selection
+      </Link>
+      <Link
+        className="font-bold text-nav pe-2 me-2 active:text-white active:underline hover:text-white"
         href="/#purchase"
       >
         Purchasing
       </Link>
-
       <Link
-        className="font-bold text-white border active:bg-[#521322] hover:bg-[#821D36] border-white py-1 px-5 "
+        className="font-bold text-nav active:text-white active:underline hover:text-white"
         href="https://docs.google.com/forms/d/1_xbRfVVhoxrf1WhNJ2NGPMpRd4FnJlNKz2uYTGnDK9A/viewform?edit_requested=true"
       >
         Contact
--- src/app/splashOffer.tsx
+++ src/app/splashOffer.tsx
@@ -2,17 +2,17 @@ import Image from "next/image";
 
 const SplashOffer: React.FC = () => {
   return (
-    <section id="offer" className="main-container">
+    <section id="selection" className="main-container">
       <h2 className="pt-24 pb-4 text-2xl text-primary text-center">
-        What we Offer
+        Our Selection
       </h2>
-      <p className="text-center w-[100%] md:w-[60%] mx-auto pb-8 text-secondary-f">
+      <p className="text-center text-lg w-[100%] md:w-[60%] mx-auto pb-8 text-secondary-f">
         We carry a unique selection of 12 student made wines and two UC Davis
         V&E label options.
       </p>
 
-      <div className="flex flex-col md:flex-row pt-8 mt-8 pb-8 mb-16 gap-16 border-primary border-b-1">
-        <div className="w-full md:w-1/2">
+      <div className="flex flex-col md:flex-row pt-8 mt-8 pb-8 mb-16 gap-16">
+        <div className="w-full md:w-1/2 pb-2 border-b-2 hover:border-primary border-transparent transition duration-200">
           <Image
             src={"./productshots/vesb.jpg"}
             width={815}
@@ -26,7 +26,7 @@ const SplashOffer: React.FC = () => {
           <h3 className="text-primary text-xl mt-2">Sauvignon Blanc</h3>
           <p>$&nbsp;50.00</p>
         </div>
-        <div className="w-full md:w-1/2">
+        <div className="w-full md:w-1/2 pb-2 border-b-2 hover:border-primary border-transparent transition duration-200">
           <Image
             src={"./productshots/vecs.jpg"}
             width={815}
