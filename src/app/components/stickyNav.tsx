689e3f847d225eb61709d0eb47ceb82638e06427
Cal <cydoval@mac.com>
2025-06-03 14:53:38 -0700

    mobile nav

--- src/app/components/stickyNav.tsx
+++ src/app/components/stickyNav.tsx
@@ -41,18 +41,19 @@ export default function StickyNav() {
         show ? "opacity-100" : "opacity-0 pointer-events-none"
       } bg-primary shadow-md`}
     >
-      <div className="flex justify-between items-center py-1 px-1 md:py-4 md:px-16">
-        <div className="w-full md:w-2/3">
+      <div className="flex flex-col md:flex-row justify-between items-center py-1 px-1 md:py-4 md:px-16">
+        <div className="w-full md:w-1/3">
           <Link href="/#">
             <Image
               src="./hilgard631-light.svg"
               width={200}
               height={40}
               alt={"Hilgard631 Logo"}
+              className="mx-auto md:ml-0"
             />
           </Link>
         </div>
-        <div className="w-full md:w-1/3 text-end">
+        <div className="w-full md:w-2/3 text-center md:text-end">
           <NavItems />
         </div>
       </div>
